e5325c34e704119ea6c4180bac004b4d90b0070f
amayaaaa24 <amayaaaa24@gmail.com>
2024-07-10 16:51:05 -0500

    correct subtract test

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -8,6 +8,6 @@ def test_add():
     assert cal.add(1, 2) == 3
 
 def test_subtract():
-    assert cal.add(3, 1) == 2
-    assert cal.add(2, 1) == 1
+    assert cal.subtract(5, 2) == 3
+    assert cal.subtract(2, 1) == 1
 
